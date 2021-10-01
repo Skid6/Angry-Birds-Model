823f4acfa644c2a891094da5c6e8d22f82e093e6
Sid <89701185+Skid6@users.noreply.github.com>
2021-10-01 13:51:33 -0400

    Add files via upload

---
+++ pigs.js
@@ -0,0 +1,25 @@
+class Pig {
+    constructor(x, y) {
+      var options = {
+          'restitution':0.8,
+          'friction':0.3,
+          'density':1.0
+      }
+      this.body = Bodies.circle(x, y, 25, options);
+      this.width = 50;
+      this.height = 50;
+      
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      var angle = this.body.angle;
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      rectMode(CENTER);
+      fill("#58e864");
+      ellipse(0, 0, this.width, this.height);
+      pop();
+    }
+  };
